1fc1376d667a75b9db318d70ecef709e47e4714a
Ardi <ardi@analisa.io>
2022-11-24 10:14:40 +0700

    add fetch api from openweathermap

--- components/weatherCard.tsx
+++ components/weatherCard.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
 const apiKey = {
         key: "555eea05f2d42452441573142de439a0",
         base: "http://api.openweathermap.org/data/2.5/"
     }
 function weatherCard() {
+    const [query, setQuery] = useState("");
+    const [weather, setWeather] = useState({});
+    const search = e => {
+        if (e.key === "Enter") {
+            fetch(`${apiKey.base}weather?q=${query}&units=metrics&APPID=${apiKey.key}`)
+            .then(res => res.json())
+            .then(result => {
+                setQuery("")
+                setWeather(result)
+                console.log(result)
+            });
+        }
+    }
     const dateBuilder = (d) => {
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -16,26 +29,33 @@ function weatherCard() {
         return `${day} ${date} ${month} ${year}`
     }
   return (
-    <div>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp-273.15 > 12) ? 'app warm' : 'app') : 'app'}>
         <div className='search-box'>
             <input 
                 type="text"
                 className='search-bar'
                 placeholder='Search City..' 
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyPress = {search}
             />
         </div>
-        <div className="location-box">
-            <div className='location'>New York City, US</div>
-            <div className='date'>{dateBuilder(new Date())}</div>
-        </div>
-        <div className='weather-box'>
-            <div className="temp">
-                15 C
+        {(typeof weather.main != 'undefined') ? (
+        <div>
+            <div className="location-box">
+                <div className='location'>{weather.name}, {weather.sys.country}</div>
+                <div className='date'>{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather">
-                Sunny
+            <div className='weather-box'>
+                <div className="temp">
+                    {Math.round((weather.main.temp-273.15))} °C
+                </div>
+                <div className="weather">
+                    <p>{weather.weather[0].main}</p>
+                </div>
             </div>
         </div>
+        ) : ('')}
     </div>
   )
 }
